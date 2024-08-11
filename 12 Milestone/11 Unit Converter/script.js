f8018702d47cb891f3ad4e8e033e7ba27c31387e
Surya <123731796+rsurya2004@users.noreply.github.com>
2024-08-11 10:58:33 +0530

    Add files via upload

---
+++ 12 Milestone/11 Unit Converter/script.js
@@ -0,0 +1,10 @@
+// Temperature Converter
+
+function unitConverter(celTemp){
+
+    let convertedTemp = ((9/5) * celTemp) + 32;
+    console.log(convertedTemp);
+
+}
+
+let fahrTemp = unitConverter(25);
