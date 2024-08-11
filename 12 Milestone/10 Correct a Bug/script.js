c268c2cfefb513b076409531231a0e287dd575df
Surya <123731796+rsurya2004@users.noreply.github.com>
2024-08-11 10:57:48 +0530

    Add files via upload

---
+++ 12 Milestone/10 Correct a Bug/script.js
@@ -0,0 +1,18 @@
+// Double Quantity of each item.
+
+let quantityOfItems = [3, 4, 6, 2, 8, 4];
+
+
+function doubleQuantity(items){
+
+    for(let i=0; i<items.length ; i++){
+
+        let twice = items[i];
+        console.log(2*twice);
+    }
+    
+}
+
+doubleQuantity(quantityOfItems);
+
+
